fe1ede9cf0864e3939e007d48a43f008adcb74ca
testuser4740 <testuser.4740@gmail.com>
2026-07-21 12:56:01 +0530

    init

--- dist/loaders/publicLoader.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.publicLoader = void 0;
-const publicLoader = async (_settings) => {
-    // Static/public asset serving can be added here if needed.
-};
-exports.publicLoader = publicLoader;
-//# sourceMappingURL=publicLoader.js.map
